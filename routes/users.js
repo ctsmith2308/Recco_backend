98301b4bd0890d4bee31c47df9a2497c1aae1bd9
Christopher Smith <ctsmith2308@gmail.com>
2017-12-28 12:53:49 -0700

    complete user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../knex')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  knex('users')
+  .select('*')
+  .then((data) => {
+    res.send(data)
+  })
 });
+/* GET user based on ID. */
+router.get('/:id', function(req, res, next){
+  let id = req.params.id
+  knex('users')
+  .select('username')
+  .where('id', id)
+  .then((data) => {
+    res.send(data)
+  })
+})
+/* POST new user. */
+router.post('/', function(req, res, next){
+  knex('users')
+  .insert(req.body,"*")
+  .then((newUser) => {
+    res.send(newUser)
+  })
+})
+/* DELETE user */
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id
+  knex('users')
+  .del()
+  .where('id', id)
+  .then((data) => {
+    let deletedUser = {
+      username: data.username,
+      hashedToken: data.hashed_token
+    }
+    res.send(deletedUser)
+  })
+})
+
 
 module.exports = router;
--- seeds/01users.js
+++ seeds/01users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'test1@test.com', hashed_token:'8iejfajknv79'},
-        {id: 2, username: 'test2@test.com', hashed_token:'090jiniyugac'},
-        {id: 3, username: 'test3@test.com', hashed_token:'90uuh645dytv'}
+        {id: 1, username: 'test1@test.com', hashed_token:'***********1'},
+        {id: 2, username: 'test2@test.com', hashed_token:'***********2'},
+        {id: 3, username: 'test3@test.com', hashed_token:'***********3'}
       ]);
     }).then(function() {
       return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));")
