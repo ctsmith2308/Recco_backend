965a76511a4017b274c100daf8d8b8e65707533a
Christopher Smith <ctsmith2308@gmail.com>
2017-12-28 10:36:40 -0700

    user table created

---
+++ migrations/20171228103219_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) =>{
+    table.increments().primary()
+    table.string('username', 255).unique()
+    table.string('hashed_token', 255).unique()
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
