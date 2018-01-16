bcc99a850083a2403c9ba569b59b59f57e0b5389
Christopher Smith <ctsmith2308@gmail.com>
2018-01-16 11:02:24 -0700

    posting to db success

--- routes/friends.js
+++ routes/friends.js
@@ -2,16 +2,29 @@ let express = require('express')
 let router = express.Router()
 let knex = require('../knex')
 
+// router.get('/:id', function(req, res, next){
+//   knex('friends')
+//   .select('dashboard.user_id','dashboard.bio','dashboard.username')
+//   .where('friends.user_id', req.params.id)
+//   .join('dashboard','friends.friend_id','dashboard.user_id')
+//   .then((response)=>{
+//     console.log('here is the response', response);
+//     res.send(response)
+//   })
+// })
+
+
 router.get('/:id', function(req, res, next){
   knex('friends')
-  .select('dashboard.user_id','dashboard.bio','dashboard.username')
   .where('friends.user_id', req.params.id)
   .join('dashboard','friends.friend_id','dashboard.user_id')
+  .join('photos','photos.user_id','dashboard.user_id')
+  .select('dashboard.user_id','dashboard.bio','dashboard.username','image_url')
   .then((response)=>{
+    console.log(response);
     res.send(response)
   })
 })
-
 router.post('/', function(req, res, next){
   let { userID, friendID } = req.body
   let postBody = {
--- routes/photos.js
+++ routes/photos.js
@@ -2,6 +2,20 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
 
+router.get('/:id', function (req, res, next){
+  let id = req.params.id
+  // console.log('here is the id', id);
+  // // console.log('im the params ID', req.params.id);
+  // console.log('made it to the route');
+  knex('photos')
+  .select('image_url')
+  .where('user_id', req.params.id)
+  .then((response)=>{
+    console.log('im the url', response[0]);
+    res.send(response[0])
+  })
+})
+
 router.post('/', function (req, res, next){
   let { userID, url }= req.body
   let postBody={
